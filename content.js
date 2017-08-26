c38964f2920069d216943f2866bb55bd3b2addeb
herumph <sfdavis0@gmail.com>
2017-08-26 08:05:26 -0400

    cleaning up code

--- content.js
+++ content.js
@@ -1,27 +1,14 @@
+//Sending generic message to background 
 chrome.extension.sendRequest({message: "action"});
 
+//Receiving message from background, for on/off 
 chrome.runtime.onMessage.addListener(
     function(request, sender) {
     var enable = request.message
-    //alert('Enable is '+enable)
     if(enable == true){
         change_title()
     }
 });
-
-//This code is so messy, but it works.
-function change_title(){
-var elements = document.getElementsByTagName('*');
-
-var url = document.URL
-var x = url.indexOf("&")
-//Getting JSON url
-if (x != -1) {
-    var new_url = 'https://www.youtube.com/oembed?url='+url.slice(0,x)+'&format=json'
-}
-else {
-    var new_url = 'https://www.youtube.com/oembed?url='+url+'&format=json'
-}
 
 //Stolen function from:
 //https://stackoverflow.com/a/22790025
@@ -32,25 +19,38 @@ function Get(yourUrl){
     return Httpreq.responseText;          
 }
 
-var json_obj = JSON.parse(Get(new_url));
-var title = json_obj.title
+function change_title(){
+    var elements = document.getElementsByTagName('*');
 
-//Replacing the title in every document element.
-//General outline stolen from Cloud-To-Butt extension. 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+    var url = document.URL
+    var x = url.indexOf("&")
+    //Getting JSON url
+    if (x != -1) {
+        var new_url = 'https://www.youtube.com/oembed?url='+url.slice(0,x)+'&format=json'
+    }
+    else {
+        var new_url = 'https://www.youtube.com/oembed?url='+url+'&format=json'
+    }
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+    var json_obj = JSON.parse(Get(new_url));
+    var title = json_obj.title
 
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(title, 'No Spoilers!');
+    //Replacing the title in every document element.
+    //General outline stolen from Cloud-To-Butt extension. 
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
 
-            if (replacedText !== text) {
+        for (var j = 0; j < element.childNodes.length; j++) {
+            var node = element.childNodes[j];
+
+            if (node.nodeType === 3) {
+                var text = node.nodeValue;
+                var replacedText = text.replace(title, 'No Spoilers!');
+
+                if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
+                }
             }
         }
     }
-}
 }
